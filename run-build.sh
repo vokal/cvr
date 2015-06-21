96769210e1e48c183287d566672aa52709d45a58
Jarrett Widman <jarrett@24m2.com>
2015-06-20 20:49:53 -0500

    sed junk

--- run-build.sh
+++ run-build.sh
@@ -4,6 +4,7 @@ npm run testcover
 
 RESULT=$?
 
+sed -i 's/\/var\/cache\/drone\/src\/github.com\/vokal\/cvr//g' coverage/lcov.info
 GITHASH="$(git rev-parse HEAD)"
 COVERAGE=$(<coverage/lcov.info)
 curl --data "token=$CVR_TOKEN&commit=$GITHASH&coverage=$COVERAGE&coveragetype=lcov" https://cvr.vokal.io/coverage
