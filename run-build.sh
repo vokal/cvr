2ea948be30f9862327f73d08cc4e454828fedc5c
Jarrett Widman <jarrett@24m2.com>
2015-06-20 20:33:00 -0500

    cover self

--- run-build.sh
+++ run-build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-npm test
+npm run testcover
 
 RESULT=$?
+
+GITHASH=git rev-parse HEAD
+COVERAGE=$(<coverage/lcov.info)
+curl --data "token=9f60266e3e1ee615124696ad17a5e288&commit=$GITHASH&coverage=$COVERAGE&coveragetype=lcov" https://cvr.vokal.io/coverage
 
 exit $RESULT
 
