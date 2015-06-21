272e4c8e88b654e4c502ceb183dfe5fd6464969e
Jarrett Widman <jarrett@24m2.com>
2015-06-20 20:42:37 -0500

    attempt 2

--- run-build.sh
+++ run-build.sh
@@ -4,7 +4,7 @@ npm run testcover
 
 RESULT=$?
 
-GITHASH=git rev-parse HEAD
+GITHASH="$(git rev-parse HEAD)"
 COVERAGE=$(<coverage/lcov.info)
 curl --data "token=$CVR_TOKEN&commit=$GITHASH&coverage=$COVERAGE&coveragetype=lcov" https://cvr.vokal.io/coverage
 
