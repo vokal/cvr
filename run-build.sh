7d41a33089e9a3f2ec21db6c2ace6f5ed9a1a0d6
Jarrett Widman <jarrett@24m2.com>
2015-06-26 13:25:47 -0500

    don't need sed anymore

--- run-build.sh
+++ run-build.sh
@@ -4,9 +4,8 @@ npm run testcover
 
 RESULT=$?
 
-sed -i 's/\/var\/cache\/drone\/src\/github.com\/vokal\/cvr\///g' coverage/lcov.info
 GITHASH="$(git rev-parse HEAD)"
-curl -F coverage=@coverage/lcov.info "https://cvr.vokal.io/coverage?token=$CVR_TOKEN&commit=$GITHASH&coveragetype=lcov"
+curl -F coverage=@coverage/lcov.info "https://cvr.vokal.io/coverage?token=$CVR_TOKEN&commit=$GITHASH&removepath=/var/cache/drone/src/github.com/vokal/cvr/&coveragetype=lcov"
 
 exit $RESULT
 
